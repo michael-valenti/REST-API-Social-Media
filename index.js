c4c32f509f0a0ef32bafb5b541531f2e1397f4ca
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-27 18:33:23 -0400

    Connect and require the dependencies. Create homepage route, and API routes.

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const helmet = require ("helmet");
+const morgan = require ("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
+//initialize dotenv
+dotenv.config();
+
+//mongoose connection
+mongoose.connect(process.env.MONGO_URL, ()=>{
+    console.log("Connected to MongoDB successfully.")
+});
+
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req, res)=>{
+        res.send("Welcome to the homepage!");
+});
+
+app.get("/users",(req, res)=>{
+    res.send("Welcome to the users page!");
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+//listen on port 3000
 app.listen(3000,()=>{
-    console.log("Server is running!");
-})
+    console.log("The server is ready to rock n roll!");
+});
