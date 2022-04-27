6ca6870d7c08187bed3f186eee054c8f6d320efb
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-27 19:58:14 -0400

    Add logic to update a users account.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
+//Update User
+router.put("/:id", async(req,res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch(err){
+                return res.status(500).json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {$set: req.body});
+            res.status(200).json("Account has been updated successfully.");
+        } catch(err){
+            return res.status(500).json(err);
+        }
 
-router.get("/", (req,res)=>{
-    res.send("Hello, this is the user route")
+    } else {
+        return res.status(403).json("You do not have permission to update this account.");
+    }
 });
+
 
 module.exports = router;
