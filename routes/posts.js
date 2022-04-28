c0b6ee3ae32a709e419d853f0c034a4d06692024
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-28 18:24:24 -0400

    Add logic to Like and Dislike posts.

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const Post = require("../models/Post");
 
 
@@ -49,8 +51,22 @@ router.delete("/:id", async (req, res)=>{
 });
 
 
-//Like a post
+//Like/dislike a post
 
+router.put("/:id/like", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("You have liked the post!");
+        } else {
+            await post.updateOne({$pull: {likes:req.body.userId}});
+            res.status(200).json("You have disliked the post");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 //Get a post
 
 //Generate timeline posts
