e066d536f14d0299a796d9d79301b32b386d0cb6
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-28 19:20:50 -0400

    Add logic to display a timeline of your posts and the posts from the users you follow.

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const req = require("express/lib/request");
 const res = require("express/lib/response");
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 
 //Create a post
@@ -71,13 +72,32 @@ router.put("/:id/like", async (req,res)=>{
 
 router.get("/:id", async (req, res)=>{
     try{
-        const post = await Post.findById(req.params.id);
+        const post = await Post.findById(req.params.id);S
         res.status(200).json(post);
     }catch(err){
         res.status(500).json(err);
     }
-})
+});
 
 //Generate timeline posts
+
+router.get("/timeline/all", async (req, res)=>{
+
+//let postArray = [];
+
+try{
+
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({userId:currentUser._id});
+    const followerPosts = await Promise.all(
+        currentUser.followers.map(friendId=>{
+            return Post.find({userId: friendId});
+        })
+    );
+    res.json(userPosts.concat(...followerPosts));
+}catch(err){
+    res.status(500).json(err);
+}
+});
 
 module.exports = router;
