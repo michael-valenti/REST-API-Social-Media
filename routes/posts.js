7628874f47b98675840593e27aaf544c5d4de6d3
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-28 18:00:36 -0400

    Add logic to Update a Post.

--- routes/posts.js
+++ routes/posts.js
@@ -16,6 +16,21 @@ router.post("/", async (req,res)=>{
 
 //Update a post
 
+router.put("/:id", async (req, res)=>{
+    try{
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+        await post.updateOne({$set:req.body});
+        res.status(200).json("Post updated successfully.");
+
+    }else{
+        res.status(403).json("You are only allowed to update your posts!");
+    }
+}catch(err){
+    res.status(500).json(err);
+}
+});
+
 //Delete a post
 
 //Like a post
