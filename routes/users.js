f1f9bece2414777d962f4b0b024cc33a8b4ffdd6
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-28 15:48:07 -0400

    Add logic for following a user.

--- routes/users.js
+++ routes/users.js
@@ -55,5 +55,32 @@ router.get("/:id", async (req, res)=>{
         return res.status(500).json(err);
     }
     });
-    
+
+
+//Follow a User
+router.put("/:id/follow", async (req, res)=>{
+    //check if users are the same (logged in user vs the one they are trying to follow)
+    if(req.body.userId !== req.params.id){
+        try{
+
+                const user = await User.findById(req.params.id);
+                const currentUser = await User.findById(req.body.userId);
+                if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$addToSet:{followers: req.body.userId}});
+                await currentUser.updateOne({$addToSet:{following: req.params.id}});
+                res.status(200).json("User has been followed.");
+                }else{
+                    res.status(403).json("You already follow this user.");
+                }
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("you cannot follow yourself!")
+    }
+});
+
+//Unfollow a User
+
+
 module.exports = router;
