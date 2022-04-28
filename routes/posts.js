8ab3ec8a0edece6585547c13f177caf9fb687cb3
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-28 17:29:16 -0400

    Initial commit.

---
+++ routes/posts.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+//Create a post
+
+
+//Update a post
+
+//Delete a post
+
+//Like a post
+
+//Get a post
+
+//Generate timeline posts
+
+module.exports = router;
